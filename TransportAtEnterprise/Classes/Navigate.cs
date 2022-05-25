caa04a945ab8358119fbb6cc16ceb7b347a90ea5
Tequila1Boy <pushkin.rodion@yandex.ru>
2022-05-25 17:17:12 +0300

    pages navigation

---
+++ TransportAtEnterprise/Classes/Navigate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace TransportAtEnterprise.Classes
+{
+    class Navigate
+    {
+        public static Frame MainFrame { get; set; }
+        public static string page { get; set; }
+    }
+}
--- TransportAtEnterprise/Windows/MainWindow.xaml.cs
+++ TransportAtEnterprise/Windows/MainWindow.xaml.cs
@@ -23,6 +23,87 @@ namespace TransportAtEnterprise
         public MainWindow()
         {
             InitializeComponent();
+            Classes.Navigate.MainFrame = frame;
+            navigation("driver");
+        }
+        private void navigation(string path)
+        {
+            if (path == "driver")
+            {
+                var driverbrush = new ImageBrush();
+                var carbrush = new ImageBrush();
+                var officebrush = new ImageBrush();
+                driverbrush.ImageSource = new BitmapImage(new Uri("../../Resources/driver-active.png", UriKind.Relative));
+                carbrush.ImageSource = new BitmapImage(new Uri("../../Resources/car.png", UriKind.Relative));
+                officebrush.ImageSource = new BitmapImage(new Uri("../../Resources/office.png", UriKind.Relative));
+                driver.Background = driverbrush;
+                car.Background = carbrush;
+                office.Background = officebrush;
+                header.Text = "Водители";
+                Classes.Navigate.page = "driver";
+                Classes.Navigate.MainFrame.NavigationService.Source = new Uri("/Pages/Driver/Driver.xaml", UriKind.Relative);
+            }
+            else if(path == "car")
+            {
+                var driverbrush = new ImageBrush();
+                var carbrush = new ImageBrush();
+                var officebrush = new ImageBrush();
+                driverbrush.ImageSource = new BitmapImage(new Uri("../../Resources/driver.png", UriKind.Relative));
+                carbrush.ImageSource = new BitmapImage(new Uri("../../Resources/car-active.png", UriKind.Relative));
+                officebrush.ImageSource = new BitmapImage(new Uri("../../Resources/office.png", UriKind.Relative));
+                driver.Background = driverbrush;
+                car.Background = carbrush;
+                office.Background = officebrush;
+                header.Text = "Автомобили";
+                Classes.Navigate.page = "car";
+                Classes.Navigate.MainFrame.NavigationService.Source = new Uri("/Pages/Car/Car.xaml", UriKind.Relative);
+            }
+            else if(path == "office")
+            {
+                var driverbrush = new ImageBrush();
+                var carbrush = new ImageBrush();
+                var officebrush = new ImageBrush();
+                driverbrush.ImageSource = new BitmapImage(new Uri("../../Resources/driver.png", UriKind.Relative));
+                carbrush.ImageSource = new BitmapImage(new Uri("../../Resources/car.png", UriKind.Relative));
+                officebrush.ImageSource = new BitmapImage(new Uri("../../Resources/office-active.png", UriKind.Relative));
+                driver.Background = driverbrush;
+                car.Background = carbrush;
+                office.Background = officebrush;
+                header.Text = "Предприятия";
+                Classes.Navigate.page = "office";
+                Classes.Navigate.MainFrame.NavigationService.Source = new Uri("/Pages/Office/Office.xaml", UriKind.Relative);
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            navigation("driver");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            navigation("car");
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            navigation("office");
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            if(Classes.Navigate.page == "driver")
+            {
+                //showdialog заблокирует текущее
+                MessageBox.Show("add driver");
+            }
+            else if (Classes.Navigate.page == "car")
+            {
+                MessageBox.Show("add car");
+            }
+            else if (Classes.Navigate.page == "office")
+            {
+                MessageBox.Show("add office");
+            }
         }
     }
 }
